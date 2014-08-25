7419943445d7d7f40d2ac06c6ed18d90486c5cb0
cesarhdz <mrshortcuts@gmail.com>
2014-08-24 22:59:09 -0500

    ImageLocator should test image existence

--- spec/CesarHdz/TinTan/ImageLocatorSpec.php
+++ spec/CesarHdz/TinTan/ImageLocatorSpec.php
@@ -2,6 +2,7 @@
 
 namespace spec\CesarHdz\TinTan;
 
+use org\bovigo\vfs\vfsStream;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
@@ -10,5 +11,33 @@ class ImageLocatorSpec extends ObjectBehavior
     function it_is_initializable()
     {
         $this->shouldHaveType('CesarHdz\TinTan\ImageLocator');
+    }
+
+
+    function it_should_test_image_existence(){
+        // Where
+        $base = 'image-existence';
+        $tree = ['tintan.jpg' => 'image mock'];
+
+        // Setup
+        $fs = $this->_fs($base, $tree);
+        $this->addDir($fs->dir);
+    		
+        // Expect
+        $this->exists('tintan.jpg')->shouldReturn(true);
+    	$this->exists('not-found.jpg')->shouldReturn(false);
+    }
+
+
+
+    function _fs($base, array $tree){
+        $stream = vfsStream::setup($base);
+
+        vfsStream::create($tree, $stream);
+
+        return (object) [
+            'stream' => $stream,
+            'dir'    => vfsStream::url($base)
+        ];
     }
 }
--- src/CesarHdz/TinTan/ImageLocator.php
+++ src/CesarHdz/TinTan/ImageLocator.php
@@ -2,6 +2,39 @@
 
 namespace CesarHdz\TinTan;
 
+use Symfony\Component\Finder\Finder;
+
 class ImageLocator
 {
+
+	protected $dirs;
+
+	public function __construct(){
+		$this->dirs = array();
+	}
+
+    public function addDir($dir)
+    {
+        $this->dirs[] = $dir;
+    }
+
+    public function exists($path)
+    {
+        $finder = $this->getFinder();
+        $finder->path($path);
+
+        return count($finder) ? true : false;
+    }
+
+
+    protected function getFinder(){
+    	$finder = new Finder();
+    	$finder->files();
+
+    	foreach ($this->dirs as $dir) {
+    		$finder->in($dir);
+    	}
+
+    	return $finder;
+    }
 }
