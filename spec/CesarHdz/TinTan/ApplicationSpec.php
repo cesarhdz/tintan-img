6c462c4cdc43eff8fecdc67595f3494d48e35577
cesarhdz <mrshortcuts@gmail.com>
2014-08-26 00:57:17 -0500

    Base dir and configuration added to Application

--- spec/CesarHdz/TinTan/ApplicationSpec.php
+++ spec/CesarHdz/TinTan/ApplicationSpec.php
@@ -4,6 +4,7 @@ namespace spec\CesarHdz\TinTan;
 
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use org\bovigo\vfs\vfsStream;
 
 class ApplicationSpec extends ObjectBehavior
 {
@@ -11,5 +12,53 @@ class ApplicationSpec extends ObjectBehavior
     {
         $this->shouldHaveType('CesarHdz\TinTan\Application');
         $this->shouldHaveType('Silex\Application');
+    }
+
+    function it_should_be_constructed_with_a_dir(){
+    	$this->beConstructedWith('/some/dir');
+
+    	$this['dir']->shouldBe('/some/dir/');
+    }
+
+
+    function it_should_read_configuration_from_file(){
+
+    	// where
+    	$config = <<<EOD
+version: '1.0'
+cache.dir: /tmp/tin-tan/
+
+filters:
+  - CesarHdz//TinTan//Thumbnail 
+  - CesarHdz//TinTan//Copyright
+EOD;
+
+		//setup
+		$fs = $this->_mock_fs('first-config', ['tintan.yml' => $config]);
+
+    	// when reading config return self to allow fluent interface
+    	$this->beConstructedWith($fs->dir);
+
+    	// and
+    	$this->bootstrap();
+
+    	// then
+    	$this['version']->shouldBe('1.0');
+    	$this['cache.dir']->shouldBe('/tmp/tin-tan/');
+    	$this['filters']->shouldBeArray();
+    }
+
+
+
+
+    function _mock_fs($base, array $tree){
+        $stream = vfsStream::setup($base);
+
+        vfsStream::create($tree, $stream);
+
+        return (object) [
+            'stream' => $stream,
+            'dir'    => vfsStream::url($base)
+        ];
     }
 }
--- src/CesarHdz/TinTan/Application.php
+++ src/CesarHdz/TinTan/Application.php
@@ -3,7 +3,47 @@
 namespace CesarHdz\TinTan;
 
 use Silex\Application as Silex;
+use Symfony\Component\Yaml\Yaml;
 
 class Application extends Silex
 {
+
+	const NAME = 'tintan';
+	const VERSION = '0.1';
+
+	protected $dir;
+
+
+	public function __construct($dir = null, $configFile = null){
+		parent::__construct();
+
+		$this->setDir($dir);
+		$this->setConfigFile($configFile);
+	}
+
+    protected function setDir($dir)
+    {
+    	$dir = $dir ?: getcwd();
+        $this['dir'] = rtrim($dir, '/') . '/';
+    }
+
+    protected function setConfigFile($file){
+    	$this['config.file'] = $file ?: self::NAME . '.yml';
+    }
+
+    public function bootstrap()
+    {
+        $this->readConfig();
+    }
+
+
+    protected function readConfig(){
+    	$file = $this['dir'] . $this['config.file'];
+
+    	$config = Yaml::parse($file);
+
+    	foreach ($config as $key => $value) {
+    		$this[$key] = $value;
+    	}
+    }
 }
