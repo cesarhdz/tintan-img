de66b1ae9334f5e9e6d9e4a675bf49321a5d341c
cesarhdz <mrshortcuts@gmail.com>
2014-08-26 00:06:15 -0500

    FilterNameMatcher allows params

--- spec/CesarHdz/TinTan/FilterNameMatcherSpec.php
+++ spec/CesarHdz/TinTan/FilterNameMatcherSpec.php
@@ -11,4 +11,47 @@ class FilterNameMatcherSpec extends ObjectBehavior
     {
         $this->shouldHaveType('CesarHdz\TinTan\FilterNameMatcher');
     }
+
+
+    function it_should_tell_me_if_a_name_matches_a_given_query_with_numeric_params(){
+    	// given
+    	$this->setPattern('%dx?%d?');
+
+    	// expect
+    	$this->match('150x150')
+    		->shouldReturn(true);
+
+    	// expect
+    	$this->match('150')
+    		->shouldReturn(true);
+    }
+
+
+    function it_should_tell_me_if_a_name_matches_a_given_query_with_string_params(){
+    	// given
+    	$this->setPattern('watermark-%s-%d');
+
+    	// expect
+    	$this->match('watermark-bottom-10')
+    		->shouldReturn(true);
+
+    	// expect
+    	$this->match('watermark-10-bottom')
+    		->shouldReturn(false);
+    }
+
+    function it_should_tell_me_if_a_name_matches_a_given_query_with_wildcard_params(){
+    	// given
+    	$this->setPattern('copy:*');
+
+    	//expect
+    	$this->match('copy:Imagen+Ideal')
+    		->shouldBe(true);
+
+    	// expect
+    	$this->match('copy:/---*/*/*-/')
+    		->shouldBe(true);
+
+    }
+
 }
--- src/CesarHdz/TinTan/FilterNameMatcher.php
+++ src/CesarHdz/TinTan/FilterNameMatcher.php
@@ -4,4 +4,41 @@ namespace CesarHdz\TinTan;
 
 class FilterNameMatcher
 {
+
+	protected $pattern;
+
+
+	/**
+	 * Set default pattern
+	 * @param String $pattern
+	 * @return  void
+	 */	
+    public function setPattern($pattern)
+    {	
+    	$pattern = preg_quote($pattern, '#');
+
+    	// convert back ? sign
+    	$pattern = str_replace('\\?', '?', $pattern);
+
+    	// convert %d into (\d+)
+    	$pattern = str_replace('%d', '(\d+)', $pattern);
+
+    	// convert %s into ([a-z]+)
+    	$pattern = str_replace('%s', '([a-zA-Z]+)', $pattern);
+
+    	$pattern = str_replace('\*', '.*', $pattern);
+    	
+    	// Set Pattern
+        $this->pattern = '#'.$pattern.'#i';
+    }
+
+    public function match($search)
+    {
+    	$match = preg_match($this->pattern, $search, $params);
+    	
+    	$this->params = (count($params)) 
+    				 ? array_shift($params) : [];
+
+        return $match == 1 ? true : false;
+    }
 }
